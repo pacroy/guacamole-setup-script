60b5858b17b30d55750a85ad66e2730351279ff6
Par <pacroy@gmail.com>
2021-04-17 22:01:52 +0700

    Add install build-essential and all dependencies

--- setup.sh
+++ setup.sh
@@ -2,4 +2,24 @@
 set -o errexit
 set -o pipefail
 
-sudo apt-get update && sudo apt-get -y upgrade
+# Update & upgrade system
+sudo apt-get update && sudo apt-get --yes upgrade
+
+# Install build tools
+sudo apt install --yes build-essential
+
+# Install build dependencies
+sudo apt install --yes libcairo2-dev libjpeg-turbo8-dev libpng-dev libtool-bin libossp-uuid-dev
+
+# Install optional dependencies
+sudo apt install --yes \	
+    libavcodec-dev libavformat-dev libavutil-dev libswscale-dev \
+    freerdp2-dev \
+    libpango1.0-dev \
+    libssh2-1-dev \
+    libtelnet-dev \
+    libvncserver-dev \
+    libwebsockets-dev \
+    libssl-dev \
+    libvorbis-dev \
+    libwebp-dev
