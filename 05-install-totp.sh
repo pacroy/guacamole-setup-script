ff9e31ca58bc421005c9c0d8940508f3546dc803
Par <pacroy@gmail.com>
2021-04-24 19:32:59 +0700

    Add install-totp extension

---
+++ 05-install-totp.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# TOTP Guacamole Extension Installation Script
+# Description: Script to install TOTP authentication extensiton for Guacamole on Ubuntu Server 18.04.
+# Author: Chairat Onyaem (Par)
+# Source: https://github.com/pacroy/guacamole-setup-script
+# References:
+# - https://guacamole.apache.org/doc/gug/totp-auth.html
+set -o errexit
+set -o pipefail
+
+# Variables
+GUAC_VERSION="${GUAC_VERSION:-1.3.0}"
+
+# Download and install TOTP extension
+curl -fLO "https://downloads.apache.org/guacamole/${GUAC_VERSION}/binary/guacamole-auth-totp-${GUAC_VERSION}.tar.gz"
+tar -xzf "guacamole-auth-totp-${GUAC_VERSION}.tar.gz"
+
+sudo mkdir -p /etc/guacamole/extensions
+sudo cp "guacamole-auth-totp-${GUAC_VERSION}/guacamole-auth-totp-${GUAC_VERSION}.jar" "/etc/guacamole/extensions/"
+
+# Restart tomcat
+sudo systemctl restart tomcat
+
+# Clean up
+rm -rf "guacamole-auth-totp-${GUAC_VERSION}"
+rm "guacamole-auth-totp-${GUAC_VERSION}.tar.gz"
