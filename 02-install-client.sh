2dbcb2b9dce882c8f3d844a5c8351ae73c423d59
Par <pacroy@gmail.com>
2021-04-28 20:29:37 +0700

    Fix echo multiline

--- 02-install-client.sh
+++ 02-install-client.sh
@@ -47,19 +47,19 @@ sudo chown -R tomcat webapps/ work/ temp/ logs/
 
 # Configure Tomcat service
 JAVA_HOME=$(update-java-alternatives -l | awk '{print $3}')
-echo "[Unit]
+echo '[Unit]
 Description=Apache Tomcat Web Application Container
 After=network.target
 
 [Service]
 Type=forking
 
-Environment=JAVA_HOME=${JAVA_HOME}
+Environment=JAVA_HOME='"${JAVA_HOME}"'
 Environment=CATALINA_PID=/opt/tomcat/temp/tomcat.pid
 Environment=CATALINA_HOME=/opt/tomcat
 Environment=CATALINA_BASE=/opt/tomcat
-Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
-Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
+Environment="CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC"
+Environment="JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom"
 
 ExecStart=/opt/tomcat/bin/startup.sh
 ExecStop=/opt/tomcat/bin/shutdown.sh
@@ -71,7 +71,7 @@ RestartSec=10
 Restart=always
 
 [Install]
-WantedBy=multi-user.target" | sudo tee /etc/systemd/system/tomcat.service > /dev/null
+WantedBy=multi-user.target' | sudo tee /etc/systemd/system/tomcat.service > /dev/null
 sudo systemctl daemon-reload
 sudo systemctl start tomcat
 systemctl status tomcat.service --no-pager
